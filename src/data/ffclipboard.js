f5e68b052e288af74b4cf02d9d4414affd85cc4d
Kevin Grigorenko <kevin@myplaceonline.com>
2015-06-16 22:14:02 -0700

    Add console.logs, test button to test.html, and support file: pages

--- src/data/ffclipboard.js
+++ src/data/ffclipboard.js
@@ -1,6 +1,25 @@
-function setText(str) {
-  self.port.emit("setText", str);
+var ffclipboard_debug = false;
+
+if (ffclipboard_debug) {
+  console.log("ffclipboard (fflipboard.js): loading");
 }
- 
+
 var ffclipboard = createObjectIn(unsafeWindow, {defineAs: "ffclipboard"});
-exportFunction(setText, ffclipboard, {defineAs: "setText"});
+
+exportFunction(function(str) {
+  
+  if (ffclipboard_debug) {
+    console.log("ffclipboard (fflipboard.js): setText called, emiting...");
+  }
+
+  self.port.emit("setText", str);
+  
+  if (ffclipboard_debug) {
+    console.log("ffclipboard (fflipboard.js): emit returned");
+  }
+  
+}, ffclipboard, {defineAs: "setText"});
+
+if (ffclipboard_debug) {
+  console.log("ffclipboard (fflipboard.js): finished loading");
+}
--- src/lib/main.js
+++ src/lib/main.js
@@ -1,13 +1,43 @@
-// FF >= 34
-var pageMod = require("sdk/page-mod");
-var clipboard = require("sdk/clipboard");
+var ffclipboard_debug = false;
+
+if (ffclipboard_debug) {
+  console.log("ffclipboard (main.js): loading");
+}
+
+const pageMod = require("sdk/page-mod");
+const clipboard = require("sdk/clipboard");
+const data = require('sdk/self').data;
+
+if (ffclipboard_debug) {
+  console.log("ffclipboard (main.js): imported dependencies");
+}
 
 pageMod.PageMod({
-  include: "*",
-  contentScriptFile: "./ffclipboard.js",
+  include: ['*', 'file:*', 'about:blank'],
+  contentScriptWhen: "start",
+  contentScriptFile: data.url("ffclipboard.js"),
   onAttach: function(worker) {
+    
+    if (ffclipboard_debug) {
+      console.log("ffclipboard (main.js): onAttach called");
+    }
+    
     worker.port.on("setText", function(str) {
+
+      if (ffclipboard_debug) {
+        console.log("ffclipboard (main.js): setText called, setting clipboard");
+      }
+    
       clipboard.set(str);
+
+      if (ffclipboard_debug) {
+        console.log("ffclipboard (main.js): clipboard set returned");
+      }
+    
     });
   }
 });
+
+if (ffclipboard_debug) {
+  console.log("ffclipboard (main.js): finished loading");
+}
